3e61ed133954a000031c237b3570b87811f0d982
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2020-04-20 18:30:00 +0100

    create Identicon.Image struct containing a hex property and add a color property to that same struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,2 +1,23 @@
 defmodule Identicon do
+  # A Struct is like a map that is used to hold data in an Elixir application
+  # It has two advantages compared to a map: it can be given default values, and has additional compile time checks
+
+  def main(input) do
+    input
+    |> hash_input()
+    |> pick_color()
+  end
+
+  @spec pick_color(Identicon.Image.t()) :: [...]
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
+  end
 end
