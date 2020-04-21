3dc9f5baf4e175e4fdd5f8f6aea702df52086fe2
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2020-04-21 10:23:36 +0100

    add a grid to the struct and make this grid only hold the tuples that have an even value

--- lib/image.ex
+++ lib/image.ex
@@ -2,5 +2,5 @@ defmodule Identicon.Image do
   # The sole purpose of this module is to manage our Struct, which is going to hold of the data we need in the
   # application in a single data structure
 
-  defstruct hex: nil, color: nil
+  defstruct hex: nil, color: nil, grid: nil
 end
